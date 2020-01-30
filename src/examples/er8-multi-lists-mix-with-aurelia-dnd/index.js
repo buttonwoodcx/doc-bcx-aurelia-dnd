17a3e4b88c59b308b4a589ccc03eacee2b6b32f2
Chunpeng Huo <huocp@me.com>
2020-01-31 09:34:53 +1100

    fix: show missing files

--- src/examples/er8-multi-lists-mix-with-aurelia-dnd/index.js
+++ src/examples/er8-multi-lists-mix-with-aurelia-dnd/index.js
@@ -2,6 +2,10 @@ export class Index {
   sources = [
     'src/examples/er8-multi-lists-mix-with-aurelia-dnd/list-container.html',
     'src/examples/er8-multi-lists-mix-with-aurelia-dnd/list-container.js',
-    'src/examples/er8-multi-lists-mix-with-aurelia-dnd/list-container.css'
+    'src/examples/er8-multi-lists-mix-with-aurelia-dnd/list-container.css',
+    'src/examples/er8-multi-lists-mix-with-aurelia-dnd/loose-item.html',
+    'src/examples/er8-multi-lists-mix-with-aurelia-dnd/loose-item.js',
+    'src/examples/er8-multi-lists-mix-with-aurelia-dnd/repository.html',
+    'src/examples/er8-multi-lists-mix-with-aurelia-dnd/repository.js'
   ]
 }
